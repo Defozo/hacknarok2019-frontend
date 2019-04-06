f54fa3ae6ed31c91f757c53e13f94a4cb57696f6
Grzegorz Tłuszcz <gtluszcz@gmail.com>
2019-04-06 18:49:22 +0200

    Finish Welcome

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="dashboard">
-        this is dashboard
+        <widget></widget>
     </div>
 </template>
 
 <script>
   // @ is an alias to /src
   // import HelloWorld from '@/components/HelloWorld.vue'
+
 
   export default {
     name: 'Dashboard'
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,31 +1,39 @@
 <template>
-    <div class="welcome">
-        this is welcome
-        <div class="container">
-            <label>File
-                <input type="file" id="file" ref="file"
-                       v-on:change="handleFileUpload()"/>
-            </label>
-        </div>
+  <div class="welcome flex flex-col min-h-screen">
+    <div class="mt-32">
+      <h1 class="text-purple-dark m-8 text-5xl">
+        Get your stats now
+      </h1>
+      <label
+        v-if="!zip"
+        class="p-4 m-4 shadow-lg text-white rounded bg-purple-dark white inline-block cursor-pointer">
+        Choose zip
+        <input type="file" accept=".zip" id="zip" ref="zip" class="hidden"
+               v-on:change="handleFileUpload()"/>
+      </label>
+      <div class="lds-ring m-4" v-else>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-  import {mapMutations} from 'vuex'
-  import {SET_ZIP} from "../store/mutations";
-
   export default {
     name: "Welcome",
     data() {
       return {
-        file: null
+        zip: null
       }
     },
     methods: {
-      ...mapMutations([SET_ZIP]),
-      handleFileUpload() {
-        const file = this.$refs.file.files[0];
-        this.setZip(file)
+      async handleFileUpload() {
+        this.zip = this.$refs.zip.files[0];
+        //TODO replace with processing zip
+        this.$router.push( '/dashboard' );
       }
     }
   }
@@ -33,5 +41,52 @@
 </script>
 
 <style scoped>
+  #zip {
+    overflow: hidden;
+  }
 
+  input[type="file"] {
+    display: none;
+  }
+
+  .lds-ring {
+    display: inline-block;
+    position: relative;
+    width: 64px;
+    height: 64px;
+  }
+
+  .lds-ring div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 51px;
+    height: 51px;
+    margin: 6px;
+    border: 6px solid #794acf;
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #794acf transparent transparent transparent;
+  }
+
+  .lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+
+  .lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+
+  .lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 </style>
