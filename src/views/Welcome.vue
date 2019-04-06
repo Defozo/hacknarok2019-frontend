980914e3332aeb72fad9c142c28e07b4d2756064
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-04-06 21:02:20 +0200

    Created the zip file handler

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,37 +1,50 @@
 <template>
-    <div class="welcome">
-        this is welcome
-        <div class="container">
-            <label>File
-                <input type="file" id="file" ref="file"
-                       v-on:change="handleFileUpload()"/>
-            </label>
-        </div>
+  <div class="welcome">
+    this is welcome
+    <div class="container">
+      <label>File
+        <input
+          type="file"
+          id="file"
+          ref="file"
+          @change="handleFileUpload()"
+        />
+      </label>
+
+      <img src="https://nolanlawson.github.io/database-comparison/kirby.gif" alt="Kirby">
     </div>
+  </div>
 </template>
 
 <script>
-  import {mapMutations} from 'vuex'
-  import {SET_ZIP} from "../store/mutations";
+  import { mapMutations } from 'vuex'
+
+  import { SET_ZIP } from "@/store/mutations";
+  import ZipHandler from '@/modules/ZipHandler'
 
   export default {
     name: "Welcome",
     data() {
       return {
-        file: null
+      file: null
       }
     },
     methods: {
       ...mapMutations([SET_ZIP]),
-      handleFileUpload() {
-        const file = this.$refs.file.files[0];
-        this.setZip(file)
+      async handleFileUpload() {
+        const file = this.$refs.file.files[0]
+        const zipHandler = new ZipHandler()
+        const wasSuccessful = await zipHandler.processFile(file)
+
+        if (! wasSuccessful) {
+          console.log('Shit happened.') // @todo handle error
+          return
+        }
+
+        const friends = await zipHandler.getFriends()
+
+        console.log(friends)
       }
     }
   }
-
 </script>
-
-<style scoped>
-
-</style>
