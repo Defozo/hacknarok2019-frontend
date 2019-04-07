25b507b0d6cdd19d9427a1e35f7d3f388d3d261b
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-04-07 02:40:29 +0200

    Display top words and emojis

--- src/widgets/WordsUsedWidget.vue
+++ src/widgets/WordsUsedWidget.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="words_used">
-    <h2 class="p-8 text-grey">Most Used Words</h2>
+  <div>
+    <div class="words_used">
+      <h2 class="p-8 text-grey">Most Used Words & Emojis</h2>
 
-    <div class="flex align-middle justify-between flex-wrap">
-      <div
-        class="mx-8 w-32 h-32 p-2 rounded-full bg-white shadow-lg"
-        v-bind:key="word.text" v-for="(word, index) in getTopWords">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">
-          <path :style="{'animation-duration': calculateDurationTime(word.amount)}" ref="path" class="dash" d="M83.5 416.5c-92-92-92-241 0-333s241-92 333 0 92 241 0 333" fill="none" stroke="#794acf" stroke-width="30" stroke-miterlimit="10"></path>
-        </svg>
-        <div class="word_amount text-purple-dark font-extrabold" v-text="status(index)"></div>
-        <div class="word_text font-medium text-grey-darker">{{word.text}}</div>
+      <div class="flex align-middle justify-between flex-wrap">
+        <div
+          class="mx-8 w-32 h-32 p-2 rounded-full bg-white shadow-lg"
+          :key="word.text"
+          v-for="(word, index) in getTop"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">
+            <path :style="{'animation-duration': calculateDurationTime(word.amount)}" ref="path" class="dash" d="M83.5 416.5c-92-92-92-241 0-333s241-92 333 0 92 241 0 333" fill="none" stroke="#794acf" stroke-width="30" stroke-miterlimit="10"></path>
+          </svg>
+          <div class="word_amount text-purple-dark font-extrabold" v-text="status(index)"></div>
+          <div class="word_text font-medium text-grey-darker">{{word.text}}</div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import _ from 'lodash'
   import Vue from 'vue'
   import { mapGetters } from 'vuex'
 
@@ -27,13 +32,16 @@
     data() {
       return {
         numbers: [],
-        statusArray: [0, 0, 0, 0, 0],
+        statusArray: [0, 0, 0, 0, 0, 0, 0, 0],
       }
     },
     computed: {
       ...mapGetters([GET_OWNER, GET_TOP_WORDS, GET_TOP_EMOJIS]),
       status() {
         return index => this.statusArray[index]
+      },
+      getTop() {
+        return _.take(_.reverse(_.sortBy([...this.getTopWords, ...this.getTopEmojis], 'amount')), 8)
       },
     },
     methods: {
@@ -56,7 +64,7 @@
       },
     },
     created() {
-      this.getTopWords.forEach((el, index) => {
+      this.getTop.forEach((el, index) => {
         const duration = this.calculateDuration(el.amount)
         const dt = duration / el.amount * 1000
 
@@ -77,10 +85,12 @@
     position: relative;
     top: -75px;
   }
+
   .word_text{
     position: relative;
     top: -45px;
   }
+
   @keyframes dash {
     from {
       stroke-dashoffset: 1109.8482666015625;
